6a706c07e594d72492752ef3d277e4e75393cdcb
picrossin <jadengoter@gmail.com>
2020-09-13 18:00:42 -0500

    Completed project.

---
+++ Assets/Scripts/NextScene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    [SerializeField] private string playerTag = "Player";
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag(playerTag))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] [Range(0.0f, 100.0f)] private float speed = 10.0f;
+    
+    private Rigidbody rigidbody;
+    private Vector3 movement;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(movement * speed);       
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        
+        movement = new Vector3(movementVector.x, 0.0f, movementVector.y);
+    }
+}
